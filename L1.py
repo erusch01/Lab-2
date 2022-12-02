91037e3cfb79128f153b49d73be09819c2c95e3c
erusch01 <116841757+erusch01@users.noreply.github.com>
2022-12-02 08:17:37 -0500

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,7 @@
+#Emily Rusch and Kaylen Nyhuis
+x=3
+print(x)
+y=x*5 + x
+print("The calculated amount is", y)
+z=1.2
+print(int(z))
---
+++ L4.py
@@ -0,0 +1,7 @@
+#Emily Rusch and Kaylen Nyhuis
+import math
+r = 4
+c_area = math.pi*r**2
+h = math.sqrt(r**2 + r**2)
+s_area = h**2
+print(c_area - s_area)
